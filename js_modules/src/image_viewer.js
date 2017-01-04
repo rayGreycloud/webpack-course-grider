a121b6941c751af5a6e77fdff68f573f852a5a4a
rayGreycloud <rayGreycloud@gmail.com>
2017-01-04 14:38:36 -0800

    Add images for build demo

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,5 +1,9 @@
+// Import images
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
 // Add css file
 import '../styles/image_viewer.css';
+
 // Create image element
 const image = document.createElement('img');
 // Set img src
