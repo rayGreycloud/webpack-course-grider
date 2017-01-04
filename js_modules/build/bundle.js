eb339379db1d620d717ed121f63c2232b034931b
rayGreycloud <rayGreycloud@gmail.com>
2017-01-04 10:37:41 -0800

    Refactor to ES2015 modules

--- js_modules/build/bundle.js
+++ js_modules/build/bundle.js
@@ -73,11 +73,14 @@
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 var sum = function sum(a, b) {
   return a + b;
 };
 
-module.exports = sum;
+exports.default = sum;
 
 /***/ },
 /* 1 */
@@ -86,9 +89,13 @@ module.exports = sum;
 "use strict";
 
 
-var sum = __webpack_require__(0);
+var _sum = __webpack_require__(0);
 
-var answer = sum(4, 6);
+var _sum2 = _interopRequireDefault(_sum);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var answer = (0, _sum2.default)(4, 6);
 
 console.log(answer);
 
--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,4 +1,4 @@
-const sum = require('./sum');
+import sum from './sum';
 
 let answer = sum(4, 6);
 
